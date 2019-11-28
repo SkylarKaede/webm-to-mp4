498cc09035f9fda2a560470860663e0664bbb9b0
スカイラー <54478253+sky-nyan@users.noreply.github.com>
2019-11-28 15:13:18 -0600

    Update webm-to-mp4.bat

--- webm-to-mp4.bat
+++ webm-to-mp4.bat
@@ -5,4 +5,4 @@ set filepath=%%~pi
 set filename=%%~ni
 set fileextension=%~x1
 )
-ffmpeg -i "%~f1" "%filedrive%\%filepath%%filename%.mp4"
+C:\scripts\ffmpeg.exe -i "%~f1" "%filedrive%\%filepath%%filename%.mp4"
